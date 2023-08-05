1558a3eac3fe1a1b3345535b48d02c073a3c6643
a.gapeevtsev <a.gapeevtsev@samolet.ru>
2023-08-05 17:51:00 +0800

    ДЗ 1 готово

---
+++ HW1/2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+int? max = null;
+
+for (int i = 1; i <= 3; i++)
+{
+  Console.WriteLine($"Введите число {i}");
+  int number = int.Parse(Console.ReadLine()!);
+
+  if (max == null || max < number)
+  {
+    max = number;
+  }
+}
+
+Console.WriteLine("Максимальное " + max);
+
+// Console.WriteLine("Введите первое число");
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите второе число");
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите третье число");
+// int number3 = int.Parse(Console.ReadLine()!);
+
+// int max = number1;
+
+// if (max < number2)
+// {
+//   max = number2;
+// }
+
+// if (max < number3)
+// {
+//   max = number3;
+// }
+
+// Console.WriteLine("Максимальное " + max);
---
+++ HW1/4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int index = 1;
+
+while (index <= number)
+{
+  if (index % 2 == 0)
+  {
+    string prefix = index > 2 ? ", " : "";
+
+    Console.Write(prefix + index);
+  }
+
+  index++;
+}
