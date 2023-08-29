22c58f12d1606b9bb4b5977a6e2126a059b258a8
a.gapeevtsev <a.gapeevtsev@samolet.ru>
2023-08-29 20:28:19 +0800

    ДЗ 4 готово

---
+++ HW4/1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int Prompt(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int MathPow(int a, int b)
+{
+  int result = 1;
+
+  while (b > 0)
+  {
+    result *= a;
+    b -= 1;
+  }
+
+  return result;
+}
+
+int numberBase = Prompt("Введите основание: ");
+int degree = Prompt("Введите степернь: ");
+
+Console.WriteLine($"Число {numberBase} в степени {degree} равно {MathPow(numberBase, degree)}");
---
+++ HW4/2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int Prompt(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int BumbersSum(int a)
+{
+  int result = 0;
+
+  while (a > 0)
+  {
+    result += a % 10;
+    a = a / 10;
+  }
+
+  return result;
+}
+
+int numberBase = Prompt("Введите число: ");
+
+Console.WriteLine($"Сумма цифр в числе {numberBase} равна {BumbersSum(numberBase)}");
