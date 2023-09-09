b309f271b5adf651bff4dc1a554fe37af951b295
artem_gapeevtsev <agapeevtsev@yandex.ru>
2023-09-09 16:56:46 +0800

    ДЗ 6 готово

---
+++ HW6/1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+int Prompt(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int CountGreaterThanZero(int[] array)
+{
+  int sum = 0;
+
+  foreach (var item in array)
+  {
+    sum += item > 0 ? 1 : 0;
+  }
+
+  return sum;
+}
+
+int[] CreateArray(int length)
+{
+  int[] array = new int[length];
+
+  for (int i = 0; i < length; i++)
+  {
+    array[i] = Prompt("Введите число: ");
+  }
+
+  return array;
+}
+
+int arrayLength = Prompt("Введите длину массива: ");
+int[] array = CreateArray(arrayLength);
+
+Console.WriteLine($"Количество чисел больше 0:  {CountGreaterThanZero(array)}");
---
+++ HW6/2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int COEFFICIENT = 0;
+int CONSTANT = 1;
+int COORD_X = 0;
+int COORD_Y = 1;
+
+double Prompt(string message)
+{
+  Console.WriteLine(message);
+  return Convert.ToDouble(Console.ReadLine()!);
+}
+
+double[] CreateLineData(int numberOfLine)
+{
+  double[] data = new double[2];
+
+  data[COEFFICIENT] = Prompt($"Введите коэффицент k{numberOfLine}");
+  data[CONSTANT] = Prompt($"Введите константу b{numberOfLine}");
+
+  return data;
+}
+
+double[] FindIntersectionPoint(double[] line1, double[] line2)
+{
+  double[] Point = new double[2];
+
+  Point[COORD_X] = (line1[CONSTANT] - line2[CONSTANT]) / (line2[COEFFICIENT] - line1[COEFFICIENT]);
+  Point[COORD_Y] = line1[COEFFICIENT] * Point[COORD_X] + line1[CONSTANT];
+
+  return Point;
+}
+
+bool IsValidLines(double[] line1, double[] line2)
+{
+  if (line1[COEFFICIENT] == line2[COEFFICIENT])
+  {
+    string errorMessage = line1[CONSTANT] == line2[CONSTANT]
+      ? "Прямые совпадают"
+      : "Прямые параллельны";
+
+    Console.WriteLine(errorMessage);
+
+    return false;
+  }
+
+  return true;
+}
+
+double[] lineData1 = CreateLineData(1);
+double[] lineData2 = CreateLineData(2);
+
+if (IsValidLines(lineData1, lineData2))
+{
+  double[] IntersectionPoint = FindIntersectionPoint(lineData1, lineData2);
+
+  Console.WriteLine($"Точка пересечения уравнений y={lineData1[COEFFICIENT]}*x+{lineData1[CONSTANT]} и y={lineData2[COEFFICIENT]}*x+{lineData2[CONSTANT]}");
+  Console.WriteLine($"имеет точки пересечения {IntersectionPoint[COORD_X]} и {IntersectionPoint[COORD_Y]}");
+}
