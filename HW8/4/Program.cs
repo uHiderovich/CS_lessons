209d00e9a972342e171c9e7a5da82390ccb27e0b
a.gapeevtsev <a.gapeevtsev@samolet.ru>
2023-09-13 10:20:55 +0800

    ДЗ 8. Задачи 4, 5

---
+++ HW8/4/Program.cs
@@ -0,0 +1,138 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int InputNum(string message)
+{
+  Console.Write(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int[] ShuffleArray(int[] array)
+{
+  Random random = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    int randomIndex = random.Next(array.Length);
+    int temp = array[i];
+    array[i] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+
+  return array;
+}
+
+int[] RamdomNumbersArray()
+{
+  int[] array = new int[90];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = i + 10;
+  }
+
+  return ShuffleArray(array);
+}
+
+int[,,] Create3DArray(int rows, int columns, int depth)
+{
+  int[,,] array = new int[rows, columns, depth];
+  int[] randomNumbers = RamdomNumbersArray();
+  int index = 0;
+
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        array[i, j, k] = randomNumbers[index++];
+      }
+    }
+  }
+
+  return array;
+}
+
+void Print3DArrayWithIndex(int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write("\n");
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+      }
+
+    }
+    Console.WriteLine();
+  }
+}
+
+void Print3DArray(int[,,] array)
+{
+  int level1 = array.GetLength(0);
+  int level2 = array.GetLength(1);
+  int level3 = array.GetLength(2);
+
+  Console.WriteLine("[");
+
+  for (int i = 0; i < level1; i++)
+  {
+    Console.WriteLine("  [");
+
+    for (int j = 0; j < level2; j++)
+    {
+      Console.Write("    [");
+
+      for (int k = 0; k < level3; k++)
+      {
+        Console.Write($"{array[i, j, k]}");
+
+        if (k < level3 - 1)
+        {
+          Console.Write(", ");
+        }
+      }
+
+      Console.Write("]");
+
+      if (j < level2 - 1)
+      {
+        Console.WriteLine(",");
+      }
+      else
+      {
+        Console.WriteLine();
+      }
+    }
+
+    Console.Write("  ]");
+
+    if (i < level1 - 1)
+    {
+      Console.WriteLine(",");
+    }
+    else
+    {
+      Console.WriteLine();
+    }
+  }
+
+  Console.WriteLine("]");
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int depth = InputNum("Введите глубину: ");
+
+int[,,] array = Create3DArray(rows, columns, depth);
+Print3DArray(array);
---
+++ HW8/5/Program.cs
@@ -0,0 +1,100 @@
+﻿/*
+Дополнительная задача
+
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+int InputNum(string message)
+{
+  Console.Write(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int[,] CreateArray(int size)
+{
+  int[,] array = new int[size, size];
+  string direction = "right";
+
+  int row = 0;
+  int col = 0;
+
+  int diff = 0;
+
+  for (int i = 1; i <= size * size; i++)
+  {
+    // Console.WriteLine($"row: {row}, col: {col}, diff: {diff}");
+    array[row, col] = i;
+
+    if (direction == "right")
+    {
+      if (col == (size - 1 - diff))
+      {
+        direction = "down";
+        row++;
+      }
+      else
+      {
+        col++;
+      }
+    }
+    else if (direction == "down")
+    {
+      if (row == (size - 1 - diff))
+      {
+        direction = "left";
+        col--;
+      }
+      else
+      {
+        row++;
+      }
+    }
+    else if (direction == "left")
+    {
+      if (col == diff)
+      {
+        direction = "top";
+        row--;
+        diff++; // меняем сдвиг только когда идем вверх, так как нужно уменьшить кол-во шагов
+      }
+      else
+      {
+        col--;
+      }
+    }
+    else if (direction == "top")
+    {
+      if (row == diff)
+      {
+        direction = "right";
+        col++;
+      }
+      else
+      {
+        row--;
+      }
+    }
+  }
+
+  return array;
+}
+
+void Print2DArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i, j]}\t");
+    Console.WriteLine();
+  }
+}
+
+// int rows = InputNum("Введите количество строк: ");
+int size = InputNum("Введите количество строк и столбцов: ");
+
+Print2DArray(CreateArray(size));
